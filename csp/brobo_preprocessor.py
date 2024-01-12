731bbbdccc662bd58f6e9c0fa4953ab495bef46e
DylanRR <dragaishis@gmail.com>
2024-01-12 11:50:29 -0600

    Documentation/small file extension change

--- csp/brobo_preprocessor.py
+++ csp/brobo_preprocessor.py
@@ -1,60 +1,111 @@
 from buildXlsFile import buildXFile
 
 def buildBroboProgram(data, bladeKerf, jobNumber, savePath, programNum = 1, fileName = None):
-    broboSticksData = buildBroboCutData(data, bladeKerf)
-    # print(f"BroboSticksData: {broboSticksData}")          #Used For Debugging
+    """
+    Builds a Brobo program by processing the raw cut data and creating an Excel file for each stick.
+
+    This function processes the raw cut data, prepares it for the Brobo machine, 
+    it then creates an Excel file for each stick using the buildXlsFile Library. 
+    The Excel files are saved in the specified save path with an optional file name.
+
+    Args:
+        data (list of lists): The raw cut data, where each inner list represents a stick and contains measurements.
+        bladeKerf (float): The width of the saw blade.
+        jobNumber (int): The job number.
+        savePath (str): The path where the Excel files will be saved.
+        programNum (int, optional): The starting program number. Defaults to 1.
+        fileName (str, optional): The name of the Excel files. If None, a default name will be used.
+
+    Dependencies:
+        buildXlsFile.py
+
+    Usage:
+        buildBroboProgram(data, bladeKerf, jobNumber, savePath, programNum = 1, fileName = None)
+    """
+    broboSticksData = _buildBroboCutData(data, bladeKerf)
     tempProgramNum = programNum
     for stickData in broboSticksData:
         xlsFile = buildXFile(stickData, tempProgramNum, jobNumber, savePath, fileName)
         xlsFile.buildSheet()
         tempProgramNum += 1
 
-#Build Brobo Cut Data
-def buildBroboCutData(data, bladeKerf):
-    data,bladeKerf = scaleData(data, bladeKerf)
+
+############ Private Functions ############
+        
+def _buildBroboCutData(data, bladeKerf):
+    """
+    Builds the cut data for Brobo machine.
+
+    Args:
+        data (list): The input data.
+        bladeKerf (float): The blade kerf value.
+
+    Returns:
+        list: The processed demand data.
+    """
+    data, bladeKerf = _scaleData(data, bladeKerf)
     demandData = []
     for stick in data:
         tempStick = []
-        #print (f"stick: {stick}")
-        stick = orderList(stick)
-        tempStick.append(buildInitialAbsVal(stick, bladeKerf))
-        stick = drop_first(stick)
-        for measurment in stick:
-            tempStick.append(ensure_negative(measurment))
+        stick = _orderList(stick)
+        tempStick.append(_buildInitialAbsVal(stick, bladeKerf))
+        stick = _drop_first(stick)
+        for measurement in stick:
+            tempStick.append(_ensure_negative(measurement))
         demandData.append(tempStick)
     return demandData
 
-def scaleData(data, bladeKerf):
-    bladeKerf = round(inchToDecimalInch(bladeKerf))
+def _scaleData(data, bladeKerf):
+    """
+    Scales the data and blade kerf to decimal inches.
+
+    Parameters:
+    data (list): The data to be scaled.
+    bladeKerf (float): The blade kerf value.
+
+    Returns:
+    tuple: A tuple containing the scaled data and blade kerf.
+    """
+    bladeKerf = round(_inchToDecimalInch(bladeKerf))
     for stick in data:
         for i, element in enumerate(stick):
-            stick[i] = round(inchToDecimalInch(element))
+            stick[i] = round(_inchToDecimalInch(element))
     return data, bladeKerf
 
-def buildInitialAbsVal(stickData, bladeKerf):
-    kerfCount = total_kerf_count(stickData)
-    kerfLength = total_kerf_length(kerfCount, bladeKerf)
-    partsLength = total_parts_length(stickData)
+def _buildInitialAbsVal(stickData, bladeKerf):
+    """
+    Calculates the initial absolute value by adding the total kerf length and total parts length.
+
+    Parameters:
+    stickData (list): List of stick data.
+    bladeKerf (float): Blade kerf value.
+
+    Returns:
+    float: The initial absolute value.
+    """
+    kerfCount = _total_kerf_count(stickData)
+    kerfLength = _total_kerf_length(kerfCount, bladeKerf)
+    partsLength = _total_parts_length(stickData)
     return abs(kerfLength + partsLength)
 
-#Helper Functions
-def inchToDecimalInch(measurement):
+############ Private Helper Functions ############
+def _inchToDecimalInch(measurement):
     return float(measurement) * 1000
 
-def orderList(list):
+def _orderList(list):
     return sorted(list)
 
-def total_parts_length(list):
+def _total_parts_length(list):
     return sum(list)
 
-def total_kerf_count(list):
+def _total_kerf_count(list):
     return (len(list) - 1)
 
-def total_kerf_length(KerfCount, KerfWidth):
+def _total_kerf_length(KerfCount, KerfWidth):
     return (KerfCount * KerfWidth)
 
-def ensure_negative(measurement):
+def _ensure_negative(measurement):
     return (abs(measurement) * -1)
 
-def drop_first(list):
+def _drop_first(list):
     return list[1:]
--- csp/buildXlsFile.py
+++ csp/buildXlsFile.py
@@ -3,6 +3,21 @@ import os
 import re
 
 class buildXFile:
+    """
+    Class for building an Excel file with specific data.
+
+    Args:
+        stickData (list): List of measurements.
+        programNumber (int): Program number.
+        jobNumber (str): Job number.
+        dirPath (str): Directory path.
+        fileName (str, optional): File name. Defaults to None.
+
+    Usage:
+        xlsFile = buildXFile(stickData, programNumber, jobNumber, dirPath, fileName) #fileName is optional
+        xlsFile.setAuthor('NAME') # Optional
+        xlsFile.buildSheet()
+    """
     def __init__(self, stickData, programNumber,  jobNumber, dirPath,  fileName = None):
         self.stickData = stickData
         self.savePath = dirPath
@@ -11,12 +26,17 @@ class buildXFile:
         self.author = 'Auto Generated'
         self.fileName = fileName
         self._checkValidSaveLocation()
-        #print(f"save path: {self.savePath + self.fileName}")
         self.workbook = xlsxwriter.Workbook(self.savePath + self.fileName)
         self.worksheet = self.workbook.add_worksheet()
 
     def buildSheet(self):
-        print (f"XLS Stick: {self.stickData}")
+        """
+        Builds the sheet by calling various methods to construct the header, author, program number, and cuts.
+        Finally, it saves the file.
+        
+        Args:
+            self: The current instance of the class.
+        """
         self._buildHeader()
         self._buildAuthor()
         self._buildProgramNumber()
@@ -24,12 +44,25 @@ class buildXFile:
         self._saveFile()
 
     def setAuthor(self, author):
+        """
+        Set the author of the file.
+
+        Args:
+            self: The current instance of the class.
+            author (str): The name of the author.
+        """
         self.author = author
 
-    def setFileName(self, fileName):
-        self.fileName = fileName
-
+    ############ Private Methods ############
     def _buildHeader(self):
+        """
+        Builds the header row in the worksheet.
+
+        The header row contains the column names for the different attributes.
+
+        Args:
+            self: The instance of the class.
+        """
         self.worksheet.write('A1', 'axis')
         self.worksheet.write('B1', 'number')
         self.worksheet.write('C1', 'demand')
@@ -38,14 +71,34 @@ class buildXFile:
         self.worksheet.write('F1', 'output')
 
     def _buildAuthor(self):
+        """
+        Builds the author information in the worksheet.
+        If the author is provided, it writes the author's name in cell G1 of the worksheet.
+
+        Args:
+            self: The current instance of the class.
+        """
         if self.author:
             self.worksheet.write('G1', ';Program Author: ' + self.author)
 
     def _buildProgramNumber(self):
+        """
+        Builds the program number in the worksheet.
+        This method writes the program number to cells A2 and B2 in the worksheet.
+
+        Args:
+            self: The current instance of the class.
+        """
         self.worksheet.write('A2', 'Pno')
         self.worksheet.write('B2', self.programNumber)
     
     def _buildCuts(self):
+        """
+        Builds the cuts in the worksheet based on the stickData.
+
+        Args:
+            self: The current instance of the class.
+        """
         tempLineNum = 3
         for measurement in self.stickData:
             self.worksheet.write('A' + str(tempLineNum), 0)
@@ -59,9 +112,19 @@ class buildXFile:
         self._buildFinalLine(tempLineNum)
 
     def _saveFile(self):
+        """
+        Closes the workbook and saves the file.
+        """
         self.workbook.close()
 
     def _buildFinalLine(self, lineNum):
+        """
+        Builds the final line in the worksheet.
+
+        Args:
+            self: The current instance of the class.
+            lineNum (int): The line number.
+        """
         self.worksheet.write('A' + str(lineNum), 0)
         self.worksheet.write('B' + str(lineNum), lineNum)
         self.worksheet.write('C' + str(lineNum), 0)
@@ -71,6 +134,18 @@ class buildXFile:
 
 
     def _checkValidSaveLocation(self):
+        """
+        Checks if the save location is valid and performs necessary actions.
+
+        This method first builds the job number path using the `_buildJobNumPath` method.
+        If the `fileName` attribute is empty, it automatically generates a file name using the `_autoGenerateFileName` method.
+        Otherwise, it processes the file name using the `_processFileName` method and checks if it contains any invalid characters.
+        If invalid characters are found, it prints a message and automatically generates a new file name.
+        Finally, it checks if a file with the same name already exists in the save path. If it does, it prints a message and generates a new file name.
+
+        Args:
+            self: The current instance of the class.
+        """
         self._buildJobNumPath()
 
         if not self.fileName:
@@ -86,22 +161,43 @@ class buildXFile:
                     self._autoGenerateFileName()
             
     def _buildJobNumPath(self):
+        """
+        Creates a directory path based on the job number and saves it to the `savePath` attribute.
+
+        If the directory path does not exist, it will be created using the `savePath` and `jobNum` attributes.
+        The `savePath` attribute will be updated to include the job number directory.
+
+        Args:
+            self: The current instance of the class.
+        """
         if not os.path.exists(self.savePath + '/' + self.jobNum):
             os.makedirs(self.savePath + '/' + self.jobNum)
         self.savePath = self.savePath + '/' + self.jobNum + '/'
     
     def _processFileName(self):
-        if not self.fileName.lower().endswith('.xls'):
-            self.fileName += '.xls'
+        """
+        Process the file name by appending '.xlsx' if it doesn't already end with it.
+
+        Args:
+            self: The current instance of the class.
+        """
+        if not self.fileName.lower().endswith('.xlsx'):
+            self.fileName += '.xlsx'
         
     def _autoGenerateFileName(self):
+        """
+        Auto-generates a unique file name for the Excel file based on the program number and the number of stick data parts.
+
+        Args:
+            self: The current instance of the class.
+        """
         print("Auto Generating File Name........................")
-        files = [f for f in os.listdir(self.savePath) if f.endswith('.xls')]
+        files = [f for f in os.listdir(self.savePath) if f.endswith('.xlsx')]
         count = len(files) + 1
 
         while True:
             count_str = str(count).zfill(3)
-            file_name = count_str + '-Program_Num_'+ str(self.programNumber) +'_'+ str(len(self.stickData)) + '_parts.xls'
+            file_name = count_str + '-Program_Num_'+ str(self.programNumber) +'_'+ str(len(self.stickData)) + '_parts.xlsx'
             print(f"File Name: {file_name}")
             if not os.path.exists(self.savePath + file_name):
                 break
